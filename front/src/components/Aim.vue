7dbaa62ec0efb4ece613e6fe08ae11f973c38903
reisuta <reisuta@reisutas-MacBook-Air.local>
2023-09-03 09:01:49 +0900

    lazygit  test

--- front/src/components/Aim.vue
+++ front/src/components/Aim.vue
@@ -1,38 +1,127 @@
 <template>
- <div class="m-3 p-2 border">
-  <div>test</div>
-  <input class="mt-2 p-2 rounded border border-blue-300" type="text" v-model="text" placeholder="test" />
-  <div>{{ text }}</div>
- </div>
+  <v-row>
+    <v-row>
+      <v-toolbar
+        color="gray"
+        dense
+        outlined
+        rounded
+        class="mt-8">
+        <v-toolbar-title
+          class="mx-auto">
+          目標一覧
+        </v-toolbar-title>
+      </v-toolbar>
+    </v-row>
+    <v-row>
+      <!-- <v-col v-for="r in res[0]" :key="r.id"> -->
+      <!--   <v-card -->
+      <!--     :loading="loading" -->
+      <!--     class="mx-auto my-12" -->
+      <!--     max-width="374" -->
+      <!--     @click="edit(r.id)" -->
+      <!--   > -->
+      <!--     <template slot="progress"> -->
+      <!--       <v-progress-linear -->
+      <!--         color="deep-purple" -->
+      <!--         height="10" -->
+      <!--         indeterminate -->
+      <!--       ></v-progress-linear> -->
+      <!--     </template> -->
+
+      <!--     <div v-if="r.difficulty==='low'"> -->
+      <!--       <v-img -->
+      <!--         height="250" -->
+      <!--         src="blue.jpeg" -->
+      <!--       ></v-img> -->
+      <!--     </div> -->
+
+      <!--     <div v-if="r.difficulty==='middle'"> -->
+      <!--       <v-img -->
+      <!--         height="250" -->
+      <!--         src="silver.jpeg" -->
+      <!--       ></v-img> -->
+      <!--     </div> -->
+
+      <!--     <div v-if="r.difficulty==='high'"> -->
+      <!--       <v-img -->
+      <!--         height="250" -->
+      <!--         src="orange.jpeg" -->
+      <!--       ></v-img> -->
+      <!--     </div> -->
+
+      <!--     <v-card-title>{{ r.title }}</v-card-title> -->
+
+      <!--     <v-card-text> -->
+      <!--       <v-row -->
+      <!--         align="center" -->
+      <!--         class="mx-0" -->
+      <!--       > -->
+      <!--         <div class="ms-4"> -->
+      <!--         </div> -->
+      <!--       </v-row> -->
+
+      <!--       <div class="my-4 text-subtitle-1"> -->
+      <!--         得られるもの -->
+      <!--       </div> -->
+
+      <!--       <div>{{ r.advantage }}</div> -->
+      <!--     </v-card-text> -->
+
+      <!--     <v-divider class="mx-4"></v-divider> -->
+
+      <!--     <v-card-title>{{ r.genre }}</v-card-title> -->
+
+      <!--     <v-card-text> -->
+      <!--       記載した月 : {{ r.started_at.slice(5, 7) }}月 -->
+      <!--     </v-card-text> -->
+      <!--   </v-card> -->
+      <!-- </v-col> -->
+    </v-row>
+  </v-row>
 </template>
 
 <script setup lang="ts">
 import { onBeforeMount, onMounted, onBeforeUpdate , onUpdated, onBeforeUnmount, onUnmounted, ref} from 'vue'
+import axios from 'axios';
 const text = ref('')
 
-onBeforeMount(() => {
-  console.log("on before mount")
-})
-
+const res = []
 onMounted(() => {
-  console.log("on mount")
+  axios.get('http://localhost:4000/api/aims')
+  .then(function (response) {
+    for (const l of response.data.records){
+      res.push(l)
+    }
+  })
+  console.log(res)
 })
-
-onBeforeUpdate(() => {
-  console.log("on before update", text.value)
-})
-
-onUpdated(() => {
-  console.log("on update", text.value)
-})
-
-onBeforeUnmount(() => {
-  console.log("on before unmounted")
-})
-
-onUnmounted(() => {
-  console.log("on unmounted")
-})
-
-console.log("on created")
+//export default {
+//  data() {
+//    return {
+ //     item:{
+  //      to: `${this.$route.params.id}`,
+   //   },
+    //  res:[]
+    //}
+//  },
+ // created() {
+  //  const res = []
+//    axios.get('/api/aims')
+ //   .then(function (response) {
+  //    for (const l of response.data.records){
+   //     console.log(l)
+    //    res.push(l)
+     // }
+//    })
+ //   this.res.push(res)
+  //},
+  //methods: {
+    //edit(id) {
+ //     console.log(this)
+  //    console.log(id)
+   //   this.$router.push(`/aims/${id}`)
+//    },
+ // }
+//} 
 </script>
---
+++ front/src/components/LifeCycle.vue
@@ -0,0 +1,38 @@
+<template>
+ <div class="m-3 p-2 border">
+  <div>test</div>
+  <input class="mt-2 p-2 rounded border border-blue-300" type="text" v-model="text" placeholder="test" />
+  <div>{{ text }}</div>
+ </div>
+</template>
+
+<script setup lang="ts">
+import { onBeforeMount, onMounted, onBeforeUpdate , onUpdated, onBeforeUnmount, onUnmounted, ref} from 'vue'
+const text = ref('')
+
+onBeforeMount(() => {
+  console.log("on before mount")
+})
+
+onMounted(() => {
+  console.log("on mount")
+})
+
+onBeforeUpdate(() => {
+  console.log("on before update", text.value)
+})
+
+onUpdated(() => {
+  console.log("on update", text.value)
+})
+
+onBeforeUnmount(() => {
+  console.log("on before unmounted")
+})
+
+onUnmounted(() => {
+  console.log("on unmounted")
+})
+
+console.log("on created")
+</script>
